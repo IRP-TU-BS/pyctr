f26c2f02b97eec0f8f993e4436bc8918a4dd8744
hdo <h.donat@tu-bs.de>
2022-09-07 00:11:40 +0200

    fix depricated warning

--- pyctcr/utils.py
+++ pyctcr/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 def hat(mat):
     return np.array([[0, -mat[2], mat[1]],
                      [mat[2], 0 , -mat[0]],
-                     [-mat[1], mat[0], 0]])
+                     [-mat[1], mat[0], 0]], dtype=np.float64)
 
 def invhat(mat):
     return np.array([mat[2,1], mat[0,2], mat[1,0]])
