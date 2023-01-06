f7aca03174e0edab5e5ea897986b4292b2e5f8b9
hdo <h.donat@tu-bs.de>
2023-01-06 01:02:44 +0100

    extend test to vispy

--- tests/test_matplotlib_renders.py
+++ tests/test_matplotlib_renders.py
@@ -11,14 +11,26 @@ from pyctcr.robots import ConcentricTubeContinuumRobot
 
 
 class RobotTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_matplotlib(self):
         test_scene = Scene(MatplotLib())
+        robot = Robot(load_continous_ctcr_model, "../example_robots/ctr_robot.yaml")
+        test_scene.add_robot('ctcr', robot)
+        beta = np.linspace(0,0.5,100)
+        #test_scene.add_arrows(0,0,0,1,1,1, 'red')
+        test_scene.show_forces(True)
+        for i,alpha in enumerate(np.linspace(0,np.pi/2,100)):
+            robot.set_config(alphas=[-alpha, alpha], betas=[0,beta[i]])
+            test_scene.update()
+        test_scene.show()
+
+    def test_vispy(self):
+        test_scene = Scene(VisPy())
         robot = Robot(load_continous_ctcr_model, "../example_robots/ctr_robot.yaml")
         test_scene.add_robot('ctcr', robot)
         beta = np.linspace(0,0.5,100)
         for i,alpha in enumerate(np.linspace(0,np.pi/2,100)):
             robot.set_config(alphas=[-alpha, alpha], betas=[0,beta[i]])
-            test_scene.update()
+        test_scene.update()
         test_scene.show()
 
 if __name__ == '__main__':
